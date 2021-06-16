108643458fde489b551b1f828f13a31a05cb023d
Can Ahmet Aydın <pectonic@gmail.com>
2021-06-16 21:39:27 +0300

    some edits in folders

--- OpenCV/first.py
+++ OpenCV/first.py
@@ -2,9 +2,9 @@ import cv2
 
 print(cv2.__version__)
 
-resim = cv2.imread("jaho.png")# dosyayi okur 
+resim = cv2.imread('E:\What-I-learned-or-I-am-learning-what\What-I-learned\OpenCV\jaho.png')# dosyayi okur 
 
-cv2.imshow("epik jaho image", resim) # dosyayi gosterir
+cv2.imshow("Epic Jaho Moment", resim) # dosyayi gosterir
 
 cv2.waitKey(0)# bekler, parametre ise kac saniye olacagi ile alakali
 # cv2.destroyWindow("epik jaho image") bu spesifik bir pencereyi kapatmaya yarariyor
