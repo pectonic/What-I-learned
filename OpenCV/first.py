4d3cca2363722761a0f547a2dde424d42e6429c9
Can Ahmet Aydın <pectonic@gmail.com>
2021-06-16 23:12:53 +0300

    tried matplotlib module

--- OpenCV/first.py
+++ OpenCV/first.py
@@ -1,11 +1,22 @@
 import cv2
-
+from matplotlib import pyplot as plt #bazi ozellikler saglayan pencere diyebiliriz
 
 print(cv2.__version__)
 
 resim = cv2.imread('E:\What-I-learned-or-I-am-learning-what\What-I-learned\OpenCV\jaho.png',0)# dosyayi okur 
 
 cv2.imshow("Epic Jaho Moment", resim) # dosyayi gosterir
+
+cv2.namedWindow('isimlenmis babus', cv2.WINDOW_NORMAL) #bu sekilde aslinda bos bir window acabiliyoruz, WINDOW_NORMAL ise window acildiktan sonra boyutunu elle degistirebilmemize yariyor
+cv2.imshow('isimlenmis babus', resim)
+
+
+plt.imshow(resim, cmap='gray')# cmap renk paleti icin cunku matplotlib rgb degil de bgr kullaniyor bu da kirmizi ile mavinin yer degistirmesi demek vs.
+plt.show()
+
+
+
+
 
 k = cv2.waitKey(0)# bekler, parametre ise kac saniye olacagi ile alakali
 
