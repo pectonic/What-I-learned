ace499a21a906796a422550bcded2bcaf7cdffdb
Can Ahmet Aydın <34618981+pectonic@users.noreply.github.com>
2021-11-17 13:11:47 +0300

    SQL
    SQL course from patika.dev

--- SQL/pSQL/from patika/odev/odev 1.sql
+++ SQL/pSQL/from patika/odev/odev 1.sql
@@ -12,32 +12,30 @@ Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden ger�
 Kolay Gelsin.
 */
 
-/*1
+-- 1#
 SELECT title, description FROM film;
 
-*/
 
 
-/*2
+-- 2#
 SELECT * FROM film
 WHERE length > 60 AND length < 75;
 
-*/
 
-/*3
+
+-- 3#
 SELECT * FROM film
 WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
 
-*/
 
-/*4
+
+-- 4#
 SELECT last_name FROM customer
 WHERE first_name = 'Mary';
 
-*/
 
-/*5
+
+-- 5#
 SELECT * FROM film
 WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
 
-*/
