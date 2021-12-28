f497375a57902067e46aa860eabe148501eca299
Can Ahmet Aydın <34618981+pectonic@users.noreply.github.com>
2021-12-28 16:15:57 +0300

    Create odev 10.sql
    quest 10 of sql lessons from patika.dev

---
+++ SQL/pSQL/from patika/odev/odev 10.sql
@@ -0,0 +1,25 @@
+/*
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+
+
+1=city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+2-customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+3-customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
+
+Kolay Gelsin.
+
+*/
+
+--#1
+SELECT city.city, country.country FROM city
+LEFT JOIN country ON country.country_id = city.country_id;
+
+--#2
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
+
+--#3
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
+FULL JOIN rental ON rental.customer_id = customer.customer_id;
