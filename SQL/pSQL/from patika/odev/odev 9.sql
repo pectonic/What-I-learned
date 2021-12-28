6094be89c75cd1c598ce23939c96ac42c141648a
Can Ahmet Aydın <34618981+pectonic@users.noreply.github.com>
2021-12-28 15:00:12 +0300

    odev 9 of sql lessons from patika.dev

---
+++ SQL/pSQL/from patika/odev/odev 9.sql
@@ -0,0 +1,27 @@
+/*
+Merhabalar,
+
+
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1-city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+2-customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+3-customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+
+Kolay Gelsin.
+
+*/
+
+--#1
+SELECT country.country_id , country.country, city.city FROM city
+INNER JOIN country on country.country_id = city.country_id;
+
+--#2
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment
+INNER JOIN customer ON customer.customer_id = payment.customer_id;
+
+--#3
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental
+INNER JOIN customer ON customer.customer_id = rental.customer_id;
