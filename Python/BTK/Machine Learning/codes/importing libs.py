86c79490311fac75d8edafd2068fe637d4796ea7
Can Ahmet Aydın <pectonic@gmail.com>
2022-05-04 19:52:20 +0300

    data importing and reading with pandas

--- Python/BTK/Machine Learning/codes/importing libs.py
+++ Python/BTK/Machine Learning/codes/importing libs.py
@@ -9,3 +9,15 @@ Created on Wed May  4 19:02:38 2022
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# data importing and reading
+
+datas = pd.read_csv('/home/pectonic/Documents/GitHub/What-I-learned/Python/BTK/Machine Learning/datas/veriler.csv')
+
+print(datas)
+
+height = datas[['boy']]
+print(height)
+
+height_and_weight = datas[['boy','kilo']]
+print(height_and_weight)
