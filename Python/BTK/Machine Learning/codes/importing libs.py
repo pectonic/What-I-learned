496048d1d2eb153c6a895943a790b4b5e95df09e
Can Ahmet Aydın <pectonic@gmail.com>
2022-05-04 19:18:58 +0300

    Starting new course about Machine Learning(ML)
    I'm back bitcheeeeees!

---
+++ Python/BTK/Machine Learning/codes/importing libs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 19:02:38 2022
+
+@author: pectonic
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
